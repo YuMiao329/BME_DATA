7ca53b8cdc42562efda38ec9ef94ecb1bbf91749
Yu Miao <ym168@duke.edu>
2021-10-11 13:16:17 -0400

    add server file

---
+++ mini_prog.py
@@ -0,0 +1,12 @@
+import requests
+
+server_name = "http://vcm-21170.vm.duke.edu:5000/"
+
+#request_json = {"name": "Yu Miao", "net_id": "ym168", "e-mail": "ym168@duke.edu"}
+#r = requests.post(server_name + "student", json=request_json)
+#if r.status_code != 200:
+#    print(r.text)
+#else:
+#    print(r.json())
+#r = requests.get(server_name + "sum")
+#print(r.text)
